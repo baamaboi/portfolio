a7730adac604c6987dfc87adde07806dc3825b6d
baamaboi <samarjiitsingh@gmail.com>
2023-01-09 00:02:19 +0530

    Init

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import Hero from "./Hero";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="mx-2 md:mx-4 lg:mx-8">
+      <Hero />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Hero.js
@@ -0,0 +1,14 @@
+const Hero = () => {
+  return (
+    <div className="flex">
+      <div className="flex w-7/12">
+        <span className="text-3xl text-blue-300">Hello, World !</span>
+      </div>
+      <div className="flex w-5/12">
+        <span className="text-3xl text-blue-300">Hello, World !</span>
+      </div>
+    </div>
+  );
+};
+
+export default Hero;
